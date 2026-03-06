67468ee1b30ccd01618e5072ab604fd0fb1e2f2e
Leonid <superdjskater@mail.ru>
2026-03-06 21:37:40 +0700

    olovebar

---
+++ Casks/olovebar.rb
@@ -0,0 +1,28 @@
+cask "olovebar" do
+    version :latest
+    sha256 :no_check
+
+    url "https://codeberg.org/sacrilegewastaken/olovebar/releases/download/latest/OLoveBar.dmg"
+    name "OLoveBar"
+    desc "Menu bar utility"
+    homepage "https://codeberg.org/sacrilegewastaken/olovebar"
+
+    postflight do
+      if File.exist?("#{staged_path}/OLoveBar.app")
+        system "xattr", "-r", "-d", "com.apple.quarantine", "#{staged_path}/OLoveBar.app"
+      end
+
+      if File.exist?("#{appdir}/OLoveBar.app")
+        system "xattr", "-r", "-d", "com.apple.quarantine", "#{appdir}/OLoveBar.app"
+      end
+    end
+
+    app "OLoveBar.app", target: "#{appdir}/OLoveBar.app"
+
+    uninstall quit: "com.sacrilege.olovebar"
+
+    zap trash: [
+      "~/Library/Preferences/com.sacrilege.olovebar.plist",
+      "~/Library/Application Support/OLoveBar",
+    ]
+  end
