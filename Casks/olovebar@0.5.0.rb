3b2e0f10511c8f36e32866bd1d5c9c7b89ef9140
Leonid <superdjskater@mail.ru>
2026-03-17 02:21:56 +0700

    Update OLoveBar to version 0.5.0

--- Casks/olovebar@0.5.0.rb
+++ Casks/olovebar@0.5.0.rb
@@ -1,8 +1,8 @@
-cask "olovebar" do
-  version :latest
+cask "olovebar@0.5.0" do
+  version "0.5.0"
   sha256 "cef0d588ed834294aca8b30a0e56b9fc07f6311414b557a17a7b2b5b7aa09546"
 
-  url "https://codeberg.org/sacrilegewastaken/olovebar/releases/download/latest/OLoveBar.dmg"
+  url "https://codeberg.org/sacrilegewastaken/olovebar/releases/download/0.5.0/OLoveBar.dmg"
   name "OLoveBar"
   desc "Menu bar utility"
   homepage "https://codeberg.org/sacrilegewastaken/olovebar"
